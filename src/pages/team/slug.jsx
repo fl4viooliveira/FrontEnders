bc59ee041eaff2fe46cfea2ffa999cf5a79951f9
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2023-03-16 08:56:53 +0000

    API call logic conclude to team/developers

--- src/pages/team/slug.jsx
+++ src/pages/team/slug.jsx
@@ -1,10 +1,29 @@
-import {useParams} from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-export default function Developer(){
-  let {slug} = useParams()
-  return(
-  <div>
-      <h1>{slug}</h1>
+export default function Developer() {
+  let { slug } = useParams();
+  const apiUrl = `https://api.fl4v.io/wp-json/wp/v2/posts?slug=${slug}`;
+
+  const [dev, setDev] = useState([]);
+
+  useEffect(() => {
+    axios.get(apiUrl).then((response) => {
+      setDev(response.data);
+    });
+  }, [apiUrl, slug]);
+
+  console.log(dev, slug);
+
+  return (
+    <div>
+      {dev.map((data) => (
+        <div key={data.id}>
+          <h2>{data.title.rendered}</h2>
+          <div dangerouslySetInnerHTML={{__html: data.content.rendered}} />
+        </div>
+      ))}
     </div>
-  )
+  );
 }
