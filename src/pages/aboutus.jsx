53b893c6703e730d11d80c560186f43813f5fc80
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-03-17 15:44:24 +0000

    API url added to aboutus page

---
+++ src/pages/aboutus.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function Aboutus() {
+    const apiUrl = `https://api.fl4v.io/wp-json/wp/v2/pages/11`;
+
+
+}
