1f5136da8d54c6b246c23f7bbcb492d5acc9a8ef
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-03-17 15:45:00 +0000

    axios.get api call added

--- src/pages/aboutus.jsx
+++ src/pages/aboutus.jsx
@@ -5,5 +5,13 @@ import axios from "axios";
 export default function Aboutus() {
     const apiUrl = `https://api.fl4v.io/wp-json/wp/v2/pages/11`;
 
+    const [about, setAbout] = useState([]);
+
+    useEffect(() => {
+        axios.get(apiUrl).then((response) => {
+            setAbout(response.data.content.rendered);
+            console.log(response.data.content.rendered)
+        })
+    });
 
 }
