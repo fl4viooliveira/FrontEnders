f9fc7319c1f2467819e4187ff3aee2583edfe6a3
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2023-03-18 21:32:58 +0000

    add layout

--- src/pages/aboutus.jsx
+++ src/pages/aboutus.jsx
@@ -1,26 +1,32 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import Container from 'react-bootstrap/Container';
-import "../styles/aboutus.css"
+import Container from "react-bootstrap/Container";
+import "../styles/aboutus.css";
+import Layout from "../components/Layout";
 
 export default function Aboutus() {
-    const apiUrl = `https://api.fl4v.io/wp-json/wp/v2/pages/11`;
+  const apiUrl = `https://api.fl4v.io/wp-json/wp/v2/pages/11`;
 
-    const [about, setAbout] = useState([]);
+  const [about, setAbout] = useState([]);
 
-    useEffect(() => {
-        axios.get(apiUrl).then((response) => {
-            setAbout(response.data.content.rendered);
-            console.log(response.data.content.rendered)
-        })
+  useEffect(() => {
+    axios.get(apiUrl).then((response) => {
+      setAbout(response.data.content.rendered);
+      console.log(response.data.content.rendered);
     });
- 
-    console.log(about)
+  });
 
-    return (
-        <Container>
-        <div dangerouslySetInnerHTML={{__html: about}} className="aboutus"></div>
-        </Container>
-    )
+  console.log(about);
+
+  return (
+    <Layout>
+      <Container>
+        <div
+          dangerouslySetInnerHTML={{ __html: about }}
+          className="aboutus border  p-3"
+        ></div>
+      </Container>
+    </Layout>
+  );
 }
