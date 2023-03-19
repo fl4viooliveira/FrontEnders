3001d7d8fa73b6f8485ed93952c844ef28bb73ae
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2023-03-19 20:37:08 +0000

    Add tilt effects on cards

--- src/pages/team/index.jsx
+++ src/pages/team/index.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Developer from "./slug.jsx";
 import Layout from "../../components/Layout.jsx";
+import Tilt from "react-parallax-tilt";
 
 export default function TeamList() {
   const [team, setTeam] = useState([]);
@@ -25,32 +26,42 @@ export default function TeamList() {
           {team.map((dev) => (
             <li key={dev.id} className="col">
               <Link to={dev.slug} element={<Developer />}>
-                <div
-                  className="card m-5 p-2 mx-auto"
-                  style={{
-                    width: "18rem",
-                    height: "18rem",
-                    backgroundColor: "blue",
-                  }}
+                <Tilt
+                  tiltMaxAngleX={40}
+                  tiltMaxAngleY={40}
+                  perspective={800}
+                  transitionSpeed={1500}
+                  scale={1.1}
+                  gyroscope={true}
                 >
-                  <img
-                    className="card-img-top"
-                    src={dev.yoast_head_json.og_image[0].url}
-                    alt={dev.title.rendered}
+                  <div
+                    className="card m-5 p-2 mx-auto"
                     style={{
-                      width: "13rem",
-                      height: "13rem",
-                      margin: "0 auto",
-                      objectFit: "contain"
-
+                      width: "18rem",
+                      height: "18rem",
+                      background: "rgba( 255, 255, 255, 0.25 )",
+                      boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
+                      backdropFilter: "blur( 4.5px )",
                     }}
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title text-center">
-                      {dev.title.rendered}
-                    </h5>
+                  >
+                    <img
+                      className="card-img-top"
+                      src={dev.yoast_head_json.og_image[0].url}
+                      alt={dev.title.rendered}
+                      style={{
+                        width: "13rem",
+                        height: "13rem",
+                        margin: "0 auto",
+                        objectFit: "contain",
+                      }}
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title text-center">
+                        {dev.title.rendered}
+                      </h5>
+                    </div>
                   </div>
-                </div>
+                </Tilt>
               </Link>
             </li>
           ))}
