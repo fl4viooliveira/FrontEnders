98061573f2c1ea100d7525375f8620c3ae0c13c0
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2023-03-19 21:50:08 +0000

    Add style on developer content

--- src/pages/team/slug.jsx
+++ src/pages/team/slug.jsx
@@ -27,202 +27,229 @@ export default function Developer() {
 
   return (
     <Layout>
-    <div className="container">
-      {dev.map((data) => (
-        <div key={data.id}>
-          <h2>{data.title.rendered}</h2>
-          <div dangerouslySetInnerHTML={{ __html: data.content.rendered }} />
+      <div className="container my-4">
+        {dev.map((data) => (
+          <div
+            key={data.id}
+            className="card mb-3 border p-2"
+            style={{ width: "100%", backgroundColor: "transparent" }}
+          >
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img
+                  className="img-fluid rounded p-3"
+                  src={data.yoast_head_json.og_image[0].url}
+                  alt={data.title.rendered}
+                />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title mb-3">{data.title.rendered}</h5>
+                  <p
+                    className="card-text"
+                    dangerouslySetInnerHTML={{ __html: data.content.rendered }}
+                  ></p>
+                </div>
+              </div>
+            </div>
 
-          <img
-            src={data.yoast_head_json.og_image[0].url}
-            alt={data.title.rendered}
-          />
+            <div className="row d-flex">
+              {data.linkedin === "" ? (
+                <></>
+              ) : (
+                <div className="col text-center">
+                  <a href={data.linkedin} target="_blank" rel="noreferrer">
+                    <AiOutlineLinkedin
+                      style={{ width: "50px", height: "50px" }}
+                    />
+                  </a>
+                </div>
+              )}
 
-          <div className= "row">
-            {data.linkedin === "" ? (
-              <></>
-            ) : (
-              <a href={data.linkedin} target="_blank" rel="noreferrer">
-                <i>
-                  <AiOutlineLinkedin />
-                </i>
-              </a>
-            )}
-
-            {data.github === "" ? (
-              <></>
-            ) : (
-              <a href={data.github} target="_blank" rel="noreferrer">
-                <i>
-                  <AiOutlineGithub />
-                </i>
-              </a>
-            )}
-            {data.stackoverflow === "" ? (
-              <></>
-            ) : (
-              <a href={data.stackoverflow} target="_blank" rel="noreferrer">
-                <i>
-                  <BsStackOverflow />
-                </i>
-              </a>
-            )}
-          </div>
-
-          <section className="container-fluid skill-box" id="skills">
-            <h1 className="text-center my-5">Skills</h1>
-            <div className="jumbotron skills-jumbo jumbotron-fluid">
-              {data.skills_git_github === "0" ? (
+              {data.github === "" ? (
                 <></>
               ) : (
-                <>
-                  <h3 className="mx-3">Git / GitHub</h3>
-                  <div className="progress m-3">
-                    <div
-                      className="progress-bar progress-bar-striped progress-bar-animated"
-                      role="progressbar"
-                      style={{ width: `${data.skills_git_github}%` }}
-                    ></div>
-                  </div>
-                </>
+                <div className="col text-center">
+                  <a href={data.github} target="_blank" rel="noreferrer">
+                    <i>
+                      <AiOutlineGithub 
+                      style={{ width: "50px", height: "50px" }}
+                      />
+                    </i>
+                  </a>
+                </div>
               )}
-              {data.skills_html_css === "0" ? (
+              {data.stackoverflow === "" ? (
                 <></>
               ) : (
-                <>
-                  <h3 className="mx-3">HTML / CSS</h3>
-                  <div className="progress m-3">
-                    <div
-                      className="progress-bar progress-bar-striped bg-success progress-bar-animated"
-                      role="progressbar"
-                      style={{ width: `${data.skills_html_css}%` }}
-                    ></div>
-                  </div>
-                </>
-              )}
-              {data.skills_javascript === "0" ? (
-                <></>
-              ) : (
-                <>
-                  <h3 className="mx-3">JavaScript</h3>
-                  <div className="progress m-3">
-                    <div
-                      className="progress-bar progress-bar-striped bg-info progress-bar-animated"
-                      role="progressbar"
-                      style={{ width: `${data.skills_javascript}%` }}
-                    ></div>
-                  </div>
-                </>
-              )}
-              {data.skills_apis === "0" ? (
-                <></>
-              ) : (
-                <>
-                  <h3 className="mx-3">Consume APIs</h3>
-                  <div className="progress m-3">
-                    <div
-                      className="progress-bar progress-bar-striped bg-warning progress-bar-animated"
-                      role="progressbar"
-                      style={{ width: `${data.skills_apis}%` }}
-                    ></div>
-                  </div>
-                </>
-              )}
-              {data.skills_react === "0" ? (
-                <></>
-              ) : (
-                <>
-                  <h3 className="mx-3">React</h3>
-                  <div className="progress m-3">
-                    <div
-                      className="progress-bar progress-bar-striped bg-danger progress-bar-animated"
-                      role="progressbar"
-                      style={{ width: `${data.skills_react}%` }}
-                    ></div>
-                  </div>
-                </>
+                <div className="col text-center">
+                  <a href={data.stackoverflow} target="_blank" rel="noreferrer">
+                    <i>
+                      <BsStackOverflow 
+                      style={{ width: "50px", height: "50px" }}
+                      />
+                    </i>
+                  </a>
+                </div>
               )}
             </div>
-          </section>
 
-          <div className="container-fluid">
-            <h1 className="mb-4">Last Jobs</h1>
+            <section className="container-fluid skill-box" id="skills">
+              <h1 className="text-center my-5">Skills</h1>
+              <div className="jumbotron skills-jumbo jumbotron-fluid">
+                {data.skills_git_github === "0" ? (
+                  <></>
+                ) : (
+                  <>
+                    <h3 className="mx-3">Git / GitHub</h3>
+                    <div className="progress m-3">
+                      <div
+                        className="progress-bar progress-bar-striped progress-bar-animated"
+                        role="progressbar"
+                        style={{ width: `${data.skills_git_github}%` }}
+                      ></div>
+                    </div>
+                  </>
+                )}
+                {data.skills_html_css === "0" ? (
+                  <></>
+                ) : (
+                  <>
+                    <h3 className="mx-3">HTML / CSS</h3>
+                    <div className="progress m-3">
+                      <div
+                        className="progress-bar progress-bar-striped bg-success progress-bar-animated"
+                        role="progressbar"
+                        style={{ width: `${data.skills_html_css}%` }}
+                      ></div>
+                    </div>
+                  </>
+                )}
+                {data.skills_javascript === "0" ? (
+                  <></>
+                ) : (
+                  <>
+                    <h3 className="mx-3">JavaScript</h3>
+                    <div className="progress m-3">
+                      <div
+                        className="progress-bar progress-bar-striped bg-info progress-bar-animated"
+                        role="progressbar"
+                        style={{ width: `${data.skills_javascript}%` }}
+                      ></div>
+                    </div>
+                  </>
+                )}
+                {data.skills_apis === "0" ? (
+                  <></>
+                ) : (
+                  <>
+                    <h3 className="mx-3">Consume APIs</h3>
+                    <div className="progress m-3">
+                      <div
+                        className="progress-bar progress-bar-striped bg-warning progress-bar-animated"
+                        role="progressbar"
+                        style={{ width: `${data.skills_apis}%` }}
+                      ></div>
+                    </div>
+                  </>
+                )}
+                {data.skills_react === "0" ? (
+                  <></>
+                ) : (
+                  <>
+                    <h3 className="mx-3">React</h3>
+                    <div className="progress m-3">
+                      <div
+                        className="progress-bar progress-bar-striped bg-danger progress-bar-animated"
+                        role="progressbar"
+                        style={{ width: `${data.skills_react}%` }}
+                      ></div>
+                    </div>
+                  </>
+                )}
+              </div>
+            </section>
+
+            <div className="container-fluid">
+              <h1 className="mb-4">Last Jobs</h1>
+              <div className="row">
+                {data.job_name_1 === "" ? (
+                  <></>
+                ) : (
+                  <div class="col-md-4 d-flex justify-content-center mb-5">
+                    <JobCard
+                      deployed={data.job_deployed_1}
+                      repo={data.job_repo_1}
+                      name={data.job_name_1}
+                      img={data.job_image_1.guid}
+                      alt={data.job_name_1}
+                    />
+                  </div>
+                )}
+                {data.job_name_2 === "" ? (
+                  <></>
+                ) : (
+                  <div class="col-md-4 d-flex justify-content-center mb-5">
+                    <JobCard
+                      deployed={data.job_deployed_2}
+                      repo={data.job_repo_2}
+                      name={data.job_name_2}
+                      img={data.job_image_2.guid}
+                      alt={data.job_name_2}
+                    />
+                  </div>
+                )}
+                {data.job_name_3 === "" ? (
+                  <></>
+                ) : (
+                  <div class="col-md-4 d-flex justify-content-center mb-5">
+                    <JobCard
+                      deployed={data.job_deployed_3}
+                      repo={data.job_repo_3}
+                      name={data.job_name_3}
+                      img={data.job_image_3.guid}
+                      alt={data.job_name_3}
+                    />
+                  </div>
+                )}
+              </div>
+            </div>
+
             <div className="row">
-              {data.job_name_1 === "" ? (
+              {data.email === "" ? (
                 <></>
               ) : (
-                <div class="col-md-4 d-flex justify-content-center mb-5">
-                  <JobCard
-                    deployed={data.job_deployed_1}
-                    repo={data.job_repo_1}
-                    name={data.job_name_1}
-                    img={data.job_image_1.guid}
-                    alt={data.job_name_1}
-                  />
-                </div>
+                <a
+                  href={`mailto:${data.email}`}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="btn btn-outline-danger btn-floating m-1"
+                  role="button"
+                >
+                  <i>
+                    {data.email}: <AiOutlineMail />
+                  </i>
+                </a>
               )}
-              {data.job_name_2 === "" ? (
+              {!data.cv_pdf ? (
                 <></>
               ) : (
-                <div class="col-md-4 d-flex justify-content-center mb-5">
-                  <JobCard
-                    deployed={data.job_deployed_2}
-                    repo={data.job_repo_2}
-                    name={data.job_name_2}
-                    img={data.job_image_2.guid}
-                    alt={data.job_name_2}
-                  />
-                </div>
-              )}
-              {data.job_name_3 === "" ? (
-                <></>
-              ) : (
-                <div class="col-md-4 d-flex justify-content-center mb-5">
-                  <JobCard
-                    deployed={data.job_deployed_3}
-                    repo={data.job_repo_3}
-                    name={data.job_name_3}
-                    img={data.job_image_3.guid}
-                    alt={data.job_name_3}
-                  />
-                </div>
+                <a
+                  href={data.cv_pdf.guid}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="btn btn-outline-danger btn-floating m-1"
+                  role="button"
+                >
+                  <i>
+                    CV: <AiOutlineFilePdf />
+                  </i>
+                </a>
               )}
             </div>
           </div>
-
-          <div className="row">
-            {data.email === "" ? <></> : (
-            <a
-              href={`mailto:${data.email}`}
-              target="_blank"
-              rel="noreferrer"
-              className="btn btn-outline-danger btn-floating m-1"
-              role="button"
-            >
-              <i>
-                {data.email}: <AiOutlineMail />
-              </i>
-            </a>
-
-            )}
-            {!data.cv_pdf ? <></> : (
-            <a
-              href={data.cv_pdf.guid}
-              target="_blank"
-              rel="noreferrer"
-              className="btn btn-outline-danger btn-floating m-1"
-              role="button"
-            >
-              <i>
-                CV: <AiOutlineFilePdf />
-              </i>
-            </a>
-            
-            )}
-          </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
     </Layout>
   );
 }
