bceeff3c54aba7d39dc78e442e7bff8ca82dfefe
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2023-03-19 22:22:08 +0000

    Services adjustments

--- src/pages/services/index.jsx
+++ src/pages/services/index.jsx
@@ -22,27 +22,32 @@ export default function ServiceList() {
         <h2 className="my-4 text-center">Offered Services</h2>
         <ul className="row g-3">
           {services.map((serv) => (
-            <li className="col" key={serv.id}>
+            <li className="col-4" key={serv.id}>
               <Link to={serv.slug} element={<Service />}>
                 <div
-                  className="card m-5 p-2 mx-auto"
-                  style={{ width: "18rem", backgroundColor: "transparent" }}
+                  className="card m-5 p-2 mx-auto border"
+                  style={{ width: "18rem", height: "18rem", backgroundColor: "transparent" }}
                 >
                   <img
                     className="card-img-top"
                     src={serv.image.guid}
                     alt={serv.title.rendered}
-                    style={{ filter: "invert(100%)" }}
+                    style={{
+                      width: "13rem",
+                      height: "13rem",
+                      margin: "0 auto",
+                      objectFit: "contain",
+                      filter: "invert(100%)",
+                    }}
                   />
                   <div className="card-body">
-                    <h5 className="card-title">{serv.title.rendered}</h5>
+                    <h5 className="card-title text-center">{serv.title.rendered}</h5>
                   </div>
                 </div>
               </Link>
             </li>
           ))}
         </ul>
-        <h1>Services</h1>
       </div>
     </Layout>
   );
